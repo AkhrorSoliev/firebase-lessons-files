d52b9476c5259160dda304ddc294c755de92d889
akhrorsoliev <akhrorwork@gmail.com>
2024-11-14 00:08:30 +0500

    lesson 4 files

--- lesson-4/src/App.jsx
+++ lesson-4/src/App.jsx
@@ -2,11 +2,28 @@
 import { useEffect, useState } from "react";
 
 // firebase
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 import { db } from "./firebase/config";
 
 function App() {
   const [transactions, setTransactions] = useState(null);
+
+  const [title, setTitle] = useState(null);
+  const [price, setPrice] = useState(null);
+
+  const hanldeSubmit = (e) => {
+    e.preventDefault();
+    addDoc(collection(db, "transactions"), {
+      title,
+      price: Number(price),
+    })
+      .then(() => alert("Success"))
+      .catch((error) => alert(error.message));
+    e.target.reset();
+    setTitle(null);
+    setPrice(null);
+  };
+
   useEffect(() => {
     const getDocuments = async () => {
       const querySnapshot = await getDocs(collection(db, "transactions"));
@@ -23,6 +40,17 @@ function App() {
   return (
     <div>
       <h1>Firebase</h1>
+      <form onSubmit={hanldeSubmit}>
+        <label>
+          <span>Title:</span>
+          <input type="text" onChange={(e) => setTitle(e.target.value)} />
+        </label>
+        <label>
+          <span>Price:</span>
+          <input type="number" onChange={(e) => setPrice(e.target.value)} />
+        </label>
+        <button>add</button>
+      </form>
       <ul>
         {transactions &&
           transactions.map((transaction) => {
